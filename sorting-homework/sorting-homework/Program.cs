c727be5822aae07731f859554cf3c55d6ff9fa38
mksigrest <mksigrest@gmail.com>
2025-11-10 14:10:41 -0500

    added arrays and initalized

--- sorting-homework/sorting-homework/Program.cs
+++ sorting-homework/sorting-homework/Program.cs
@@ -7,7 +7,8 @@ namespace sortingHomework
     {
         static void Main(string[] args)
         {
-
+            int[] array1 = new int[9] { -11, 12, -42, 0, 1, 90, 68, 6, -9 };
+            int[] array2 = new int[9] { -11, 12, -42, 0, 1, 90, 68, 6, -9 };
         }
     }
 }
