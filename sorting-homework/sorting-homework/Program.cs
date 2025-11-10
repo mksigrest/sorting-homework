f28ce6277a4f1c9b16dd4d9413ae9d5342b32e7c
mksigrest <mksigrest@gmail.com>
2025-11-10 14:20:35 -0500

    printed init array1, start of sort and print array function

--- sorting-homework/sorting-homework/Program.cs
+++ sorting-homework/sorting-homework/Program.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
 
 namespace sortingHomework
 {
@@ -7,8 +6,28 @@ namespace sortingHomework
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Hello, World");
+            Console.WriteLine("Check out my awesome bubble sort!");
+
             int[] array1 = new int[9] { -11, 12, -42, 0, 1, 90, 68, 6, -9 };
             int[] array2 = new int[9] { -11, 12, -42, 0, 1, 90, 68, 6, -9 };
+
+            Console.WriteLine("The contents of the array 'array1'");
+            for (int i = 0; i < array1.Length; i++)
+            {
+                Console.WriteLine(array1[i]);
+            }
+        }
+
+        static void bubbleSort(int[] array)
+        {
+            int n = array.Length;
+
+        }
+
+        static void printArray(int[] array)
+        {
+
         }
     }
 }
